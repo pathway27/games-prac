63c14321f1914b9e702ccb3b03696e35a2875b87
pathway27 <pathway27@gmail.com>
2016-02-01 23:26:14 +1100

    flappybird: added finished tutorial

---
+++ flappy-demo/html/src/com/sree/flappy/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.sree.flappy.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sree.flappy.FlappyDemo;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new FlappyDemo();
+        }
+}
